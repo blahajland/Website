ea6ef85dd179adffb13d518225b3538a7c460f2f
Nerd <imalonelynerd@gmail.com>
2024-06-05 22:38:36 +0200

    Added extra bundles

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,14 @@
 <script setup lang="ts">
-import BlahajButton from '@/components/buttons/BlahajButton.vue'
 import TopBar from '@/components/containers/TopBar.vue'
 import CustomFooter from '@/components/CustomFooter.vue'
-import links from '@/assets/json/links.json'
 import TabGroup from '@/components/containers/TabGroup.vue'
 import HomeTabs from '@/views/tabs/HomeTabs.vue'
 import RulesTabs from '@/views/tabs/RulesTabs.vue'
 import NavbarFrame from '@/components/frames/NavbarFrame.vue'
 
-import { assets, changeLoc, themeService } from 'blahaj-library'
+import { themeService } from 'blahaj-library'
+import FooterBundle from '@/bundles/FooterBundle.vue'
+import NavbarBundle from '@/bundles/NavbarBundle.vue'
 
 themeService.setThemeFromCookie()
 </script>
@@ -16,38 +16,7 @@ themeService.setThemeFromCookie()
 <template>
   <NavbarFrame>
     <TopBar>
-      <BlahajButton
-        background="var(--background)"
-        hover="var(--surface1)"
-        @click="changeLoc(links.donate)"
-      >
-        <img alt="Donate" :src="assets.images.icons.get('donate')" />
-        <p class="displayOnDesktop">Donate</p>
-      </BlahajButton>
-      <BlahajButton
-        background="var(--background)"
-        hover="var(--surface1)"
-        @click="changeLoc(links.signup)"
-      >
-        <img alt="Donate" :src="assets.images.icons.get('signup')" />
-        <p class="displayOnDesktop">Sign up</p>
-      </BlahajButton>
-      <BlahajButton
-        background="var(--background)"
-        hover="var(--surface1)"
-        @click="changeLoc(links.login)"
-      >
-        <img alt="Donate" :src="assets.images.icons.get('login')" />
-        <p class="displayOnDesktop">Log in</p>
-      </BlahajButton>
-      <BlahajButton
-        background="var(--background)"
-        hover="var(--surface1)"
-        @click="themeService.switchTheme()"
-      >
-        <img alt="Donate" :src="assets.images.icons.get('theme')" />
-        <p class="displayOnDesktop">Theme</p>
-      </BlahajButton>
+      <NavbarBundle />
     </TopBar>
     <TabGroup>
       <HomeTabs v-if="$route.name === 'home'" />
@@ -56,10 +25,6 @@ themeService.setThemeFromCookie()
   </NavbarFrame>
   <RouterView></RouterView>
   <CustomFooter>
-    <p>&copy; <b>eryn</b> Some rights reserved</p>
-    <p>Website by <b>Blahaj Team</b></p>
-    <RouterLink to="/credits"> Credits </RouterLink>
-    <RouterLink to="/rules"> Rules & TOS </RouterLink>
-    <a :href="links.status"> Apps status </a>
+    <FooterBundle />
   </CustomFooter>
 </template>
---
+++ src/bundles/NavbarBundle.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+import { assets, changeLoc, themeService } from 'blahaj-library'
+import links from '@/assets/json/links.json'
+import BlahajButton from '@/components/buttons/BlahajButton.vue'
+</script>
+
+<template>
+  <BlahajButton
+    background="var(--background)"
+    hover="var(--surface1)"
+    @click="changeLoc(links.donate)"
+  >
+    <img alt="Donate" :src="assets.images.icons.get('donate')" />
+    <p class="displayOnDesktop">Donate</p>
+  </BlahajButton>
+  <BlahajButton
+    background="var(--background)"
+    hover="var(--surface1)"
+    @click="changeLoc(links.signup)"
+  >
+    <img alt="Donate" :src="assets.images.icons.get('signup')" />
+    <p class="displayOnDesktop">Sign up</p>
+  </BlahajButton>
+  <BlahajButton
+    background="var(--background)"
+    hover="var(--surface1)"
+    @click="changeLoc(links.login)"
+  >
+    <img alt="Donate" :src="assets.images.icons.get('login')" />
+    <p class="displayOnDesktop">Log in</p>
+  </BlahajButton>
+  <BlahajButton
+    background="var(--background)"
+    hover="var(--surface1)"
+    @click="themeService.switchTheme()"
+  >
+    <img alt="Donate" :src="assets.images.icons.get('theme')" />
+    <p class="displayOnDesktop">Theme</p>
+  </BlahajButton>
+</template>
